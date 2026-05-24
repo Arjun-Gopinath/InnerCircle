efaa53915d3a60398ca4ba25e906640367f7014c
Arjun G <arjungopinath06@gmail.com>
2026-05-24 11:22:34 +0530

    feat: add InnerCircle backend application entry point and config

---
+++ backend/src/main/java/com/arjun/innercircle/InnerCircleApplication.java
@@ -0,0 +1,11 @@
+package com.arjun.innercircle;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InnerCircleApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(InnerCircleApplication.class, args);
+    }
+}
